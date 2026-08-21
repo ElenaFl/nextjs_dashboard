9064bbe28cb8b77435fddd0c3c9a9d5e58e4e5d2
Елена <n.a.092024@yandex.ru>
2026-08-21 21:23:44 +0300

    remove lusitana from cards

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -3,7 +3,7 @@ import {
   ClockIcon,
   UserGroupIcon,
   InboxIcon,
-} from '@heroicons/react/24/outline';
+} from "@heroicons/react/24/outline";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -36,7 +36,7 @@ export function Card({
 }: {
   title: string;
   value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  type: "invoices" | "customers" | "pending" | "collected";
 }) {
   const Icon = iconMap[type];
 
@@ -46,9 +46,7 @@ export function Card({
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
-      <p
-        className="truncate rounded-xl bg-white px-4 py-8 text-center text-2xl"
-      >
+      <p className="truncate rounded-xl bg-white px-4 py-8 text-center text-2xl">
         {value}
       </p>
     </div>
